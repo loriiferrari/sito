368ab80f9094c08241bcd4570d17166bab660ce8
loriiferrari <loryf99@gmail.com>
2018-07-07 09:59:18 +0200

    sito

---
+++ nav.php
@@ -0,0 +1,10 @@
+	<nav>
+		<ul>
+			<li><a href="index.html">Introduzione</a></li>
+			<li><a href="storia.html">Storia</a></li>
+			<li><a href="sistemi.html">Sistemi e Reti</a></li>
+			<li><a href="italiano.html"><li>Italiano</li></a>
+			<li><a href="Gpoi.html"><li>Gestione Progetto e Organizzazione d'Impresa</li></a>
+		</ul>
+		<div class="clear"></div>
+	</nav>
